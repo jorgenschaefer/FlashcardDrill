8c982d6df4a753fde95d35cf351b15c210967637
Jorgen Schaefer <Jorgen.Schaefer@gmail.com>
2017-07-26 19:20:58 +0200

    Add Drill tests

--- app/src/test/java/de/jorgenschaefer/flashcarddrill/drill/DrillTest.java
+++ app/src/test/java/de/jorgenschaefer/flashcarddrill/drill/DrillTest.java
@@ -1,13 +1,143 @@
 package de.jorgenschaefer.flashcarddrill.drill;
 
 import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.jorgenschaefer.flashcarddrill.db.Card;
+
+import static junit.framework.Assert.assertTrue;
+import static junit.framework.TestCase.assertFalse;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 
 public class DrillTest {
-    CardRepository db;
+    CardRepository repository;
+    Drill drill;
+    private boolean changeListenerDidRun;
 
     @Before
     public void setUp() {
-        db = new MockCardRepository();
+        repository = new MockCardRepository();
+        drill = null;
+        changeListenerDidRun = false;
     }
 
+    private Drill getDrill() {
+        if (drill == null) {
+            drill = new Drill(repository);
+            drill.setOnChangeListener(new Runnable() {
+                @Override
+                public void run() {
+                    changeListenerDidRun = true;
+                }
+            });
+        }
+        return drill;
+    }
+
+    @Test
+    public void testConstructor() {
+        repository.insertOrUpdateCard(new Card(1, "Q", "A"));
+
+        assertEquals(getDrill().getCurrentDeck(), 0);
+        assertArrayEquals(getDrill().getDeckSizes(), new int[]{1, 0, 0, 0, 0});
+        assertTrue(getDrill().hasCards());
+        assertEquals(getDrill().getCurrentQuestion(), "Q");
+        assertEquals(getDrill().getCurrentAnswer(), "A");
+    }
+
+    @Test
+    public void testForEmptyDrill() {
+        assertEquals(getDrill().getCurrentDeck(), 0);
+        assertArrayEquals(getDrill().getDeckSizes(), new int[]{0, 0, 0, 0, 0});
+        assertFalse(getDrill().hasCards());
+    }
+
+    @Test
+    public void setCurrentDeck() {
+        Card c1 = new Card(1, "Q1", "");
+        Card c2 = new Card(2, "Q2", "");
+        repository.moveCard(c1, 0);
+        repository.moveCard(c2, 1);
+
+        getDrill().setCurrentDeck(1);
+
+        assertTrue(changeListenerDidRun);
+        assertEquals(getDrill().getCurrentQuestion(), "Q2");
+    }
+
+    @Test
+    public void onAnswerCorrect() {
+        Card c1 = new Card(1, "Q1", "");
+        Card c2 = new Card(2, "Q2", "");
+        repository.moveCard(c1, 0);
+        repository.moveCard(c2, 0);
+        String currentQuestion = getDrill().getCurrentQuestion();
+
+        getDrill().onAnswerCorrect();
+
+        assertTrue(changeListenerDidRun);
+        assertEquals(getDrill().getCurrentDeck(), 0);
+        getDrill().setCurrentDeck(1);
+        assertEquals(getDrill().getCurrentQuestion(), currentQuestion);
+    }
+
+    @Test
+    public void testOnAnswerIncorrect() {
+        Card c1 = new Card(1, "Q1", "");
+        repository.moveCard(c1, 1);
+
+        getDrill().onAnswerIncorrect();
+
+        assertTrue(changeListenerDidRun);
+        assertEquals(getDrill().getCurrentDeck(), 0);
+        assertEquals(getDrill().getCurrentQuestion(), "Q1");
+    }
+
+    @Test
+    public void onLoadCards() {
+        Card c1 = new Card(1, "Q1", "");
+        Card c2 = new Card(2, "Q2", "");
+        Card c3 = new Card(3, "Q3", "");
+        repository.moveCard(c1, 1);
+        repository.moveCard(c2, 2);
+        List<Card> cards = new ArrayList<>();
+        cards.add(c1);
+        cards.add(c2);
+        cards.add(c3);
+
+        getDrill().onLoadCards(cards);
+
+        assertFalse(changeListenerDidRun);
+        getDrill().setCurrentDeck(0);
+        assertEquals(getDrill().getCurrentQuestion(), "Q3");
+        getDrill().setCurrentDeck(1);
+        assertEquals(getDrill().getCurrentQuestion(), "Q1");
+        getDrill().setCurrentDeck(2);
+        assertEquals(getDrill().getCurrentQuestion(), "Q2");
+    }
+
+    @Test
+    public void onLoadCardsRunsChangeListenerOnEmptyDecks() {
+        Card c1 = new Card(1, "Q1", "");
+        List<Card> cards = new ArrayList<>();
+        cards.add(c1);
+
+        getDrill().onLoadCards(cards);
+
+        assertTrue(changeListenerDidRun);
+    }
+
+    @Test
+    public void onClearCards() {
+        Card c1 = new Card(1, "Q1", "");
+        repository.moveCard(c1, 1);
+
+        getDrill().onClearCards();
+
+        assertFalse(getDrill().hasCards());
+    }
 }
--- app/src/test/java/de/jorgenschaefer/flashcarddrill/drill/MockCardRepository.java
+++ app/src/test/java/de/jorgenschaefer/flashcarddrill/drill/MockCardRepository.java
@@ -6,56 +6,51 @@ import java.util.List;
 import de.jorgenschaefer.flashcarddrill.db.Card;
 
 public class MockCardRepository implements CardRepository {
-    private int[] deckSizes;
-    private List<CardMove> cardMoves = new ArrayList<>();
-    private List<Card> cards = new ArrayList<>();
-    private int numCleared = 0;
-    private List<Card> deck;
+    private int NUM_DECKS = 5;
+    private List<List<Card>> decks;
 
-    public class CardMove {
-        Card card;
-        int deck;
-
-        public CardMove(Card card, int deck) {
-            this.card = card;
-            this.deck = deck;
-        }
-    }
-
-    public void setDeckSizes(int[] deckSizes) {
-        this.deckSizes = deckSizes;
-    }
-
-    public int getNumCleared() {
-        return numCleared;
-    }
-
-    public void setDeck(List<Card> deck) {
-        this.deck = deck;
+    public MockCardRepository() {
+        clearCards();
     }
 
     @Override
     public int[] getDeckSizes() {
+        int[] deckSizes = new int[decks.size()];
+        for (int i = 0; i < deckSizes.length; i++) {
+            deckSizes[i] = decks.get(i).size();
+        }
         return deckSizes;
     }
 
     @Override
-    public void moveCard(Card card, int deck) {
-        cardMoves.add(new CardMove(card, deck));
+    public void moveCard(Card card, int deckNum) {
+        for (List<Card> deck : decks) {
+            deck.remove(card);
+        }
+        decks.get(deckNum).add(card);
     }
 
     @Override
     public void insertOrUpdateCard(Card card) {
-        cards.add(card);
+        for (List<Card> deck : decks) {
+            if (deck.remove(card)) {
+                deck.add(card);
+                return;
+            }
+        }
+        decks.get(0).add(card);
     }
 
     @Override
     public void clearCards() {
-        numCleared++;
+        decks = new ArrayList<>(NUM_DECKS);
+        for (int i = 0; i < NUM_DECKS; i++) {
+            decks.add(new ArrayList<Card>());
+        }
     }
 
     @Override
     public List<Card> getDeck(int currentDeck) {
-        return deck;
+        return decks.get(currentDeck);
     }
 }
